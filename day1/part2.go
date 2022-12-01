f83bf468e93b263ec04c4fadebc1273e1c18c500
Gabor Javorszky <gabor@javorszky.co.uk>
2022-12-01 10:20:55 +0000

    Add day 1 task 2

--- day1/part2.go
+++ day1/part2.go
@@ -1,1 +1,44 @@
 package day1
+
+import (
+	"os"
+	"sort"
+
+	"github.com/rs/zerolog"
+
+	"github.com/javorszky/adventofcode2022/inputs"
+)
+
+func Task2(l zerolog.Logger) {
+	localLogger := l.With().Int("day", 1).Int("part", 2).Logger()
+
+	gog, err := inputs.GroupByBlankLines("day1/input1.txt")
+	if err != nil {
+		localLogger.Err(err).Msg("could not read input file")
+		os.Exit(1)
+	}
+
+	gogi, err := convertToInts(gog)
+	if err != nil {
+		localLogger.Err(err).Msg("converting group of lines to group of ints")
+		os.Exit(1)
+	}
+
+	rolledUp := make([]int, len(gogi))
+	for i, g := range gogi {
+		rolledUp[i] = reduceInts(g)
+	}
+
+	sort.Ints(rolledUp)
+
+	topThree := []int{
+		rolledUp[len(rolledUp)-1],
+		rolledUp[len(rolledUp)-2],
+		rolledUp[len(rolledUp)-3],
+	}
+
+	solution := reduceInts(topThree)
+
+	s := localLogger.With().Int("solution", solution).Logger()
+	s.Info().Msgf("The three elves carrying the most calories are carrying a total of %d", solution)
+}
--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"os"
-
 	"github.com/javorszky/adventofcode2022/day1"
 
 	"github.com/rs/zerolog"
 )
 
 func main() {
-	// UNIX Time is faster and smaller than most timestamps
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-
-	l := zerolog.New(os.Stderr).With().Str("module", "adventofcode").Int("year", 2022).Logger()
+	l := zerolog.New(zerolog.NewConsoleWriter()).With().Timestamp().Str("module", "adventofcode").Int("year", 2022).Logger()
 	l.Info().Msg("Welcome to Gabor Javorszky's Advent of Code 2022 solutions!")
 
 	day1.Task1(l)
+	day1.Task2(l)
 }
