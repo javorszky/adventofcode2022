81c01aa8ebb3413acde4d2578adc4453ed14412f
Gabor Javorszky <gabor@javorszky.co.uk>
2022-12-01 09:46:17 +0000

    Add zerolog

--- day1/part1.go
+++ day1/part1.go
@@ -2,15 +2,20 @@ package day1
 
 import (
 	"fmt"
-	"log"
+	"os"
+
+	"github.com/rs/zerolog"
 
 	"github.com/javorszky/adventofcode2022/inputs"
 )
 
-func Task1() {
-	gog, err := inputs.GroupByBlankLines("day1/input1_example.txt")
+func Task1(l zerolog.Logger) {
+	localLogger := l.With().Int("day", 1).Int("part", 1).Logger()
+
+	gog, err := inputs.GroupByBlankLines("day1/inpdut1_example.txt")
 	if err != nil {
-		log.Fatalf("day 1 task 1 could not read info: %s", err.Error())
+		localLogger.Err(err).Msg("could not read input file")
+		os.Exit(1)
 	}
 	fmt.Printf("%#v", gog)
 }
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "github.com/javorszky/adventofcode2022/day1"
+import (
+	"os"
+
+	"github.com/javorszky/adventofcode2022/day1"
+
+	"github.com/rs/zerolog"
+)
 
 func main() {
-	day1.Task1()
+	// UNIX Time is faster and smaller than most timestamps
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	l := zerolog.New(os.Stderr).With().Str("module", "adventofcode").Int("year", 2022).Logger()
+	l.Info().Msg("Welcome to Gabor Javorszky's Advent of Code 2022 solutions!")
+
+	day1.Task1(l)
 }
