0cd3b9be8d7554bc9bfb37eddb1107c5e69f5e4f
Gabor Javorszky <gabor@javorszky.co.uk>
2022-12-03 18:06:25 +0000

    Remove a comment from the run command

--- cmd/run.go
+++ cmd/run.go
@@ -1,13 +1,8 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
-	"fmt"
+	"github.com/javorszky/adventofcode2022/day3"
 
-	"github.com/javorszky/adventofcode2022/day1"
-	"github.com/javorszky/adventofcode2022/day2"
 	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
 )
@@ -23,15 +18,14 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("run called")
-
 		l := zerolog.New(zerolog.NewConsoleWriter()).With().Timestamp().Str("module", "adventofcode").Int("year", 2022).Logger()
 		l.Info().Msg("Welcome to Gabor Javorszky's Advent of Code 2022 solutions!")
 
-		day1.Task1(l)
-		day1.Task2(l)
-		day2.Task1(l)
-		day2.Task2(l)
+		//day1.Task1(l)
+		//day1.Task2(l)
+		//day2.Task1(l)
+		//day2.Task2(l)
+		day3.Task1(l)
 	},
 }
 
