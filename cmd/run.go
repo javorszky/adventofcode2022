f03bfb572d1b1a7b5e3135f3f385e976f0171637
Gabor Javorszky <gabor@suborbital.dev>
2022-12-05 10:55:45 +0000

    Add day 5 stub

--- cmd/run.go
+++ cmd/run.go
@@ -1,7 +1,7 @@
 package cmd
 
 import (
-	"github.com/javorszky/adventofcode2022/day4"
+	"github.com/javorszky/adventofcode2022/day5"
 
 	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
@@ -21,15 +21,17 @@ to quickly create a Cobra application.`,
 		l := zerolog.New(zerolog.NewConsoleWriter()).With().Timestamp().Str("module", "adventofcode").Int("year", 2022).Logger()
 		l.Info().Msg("Welcome to Gabor Javorszky's Advent of Code 2022 solutions!")
 
-		//day1.Task1(l)
-		//day1.Task2(l)
-		//day2.Task1(l)
-		//day2.Task2(l)
-		//day3.Task2(l)
-		//day3.Task1(l)
+		// day1.Task1(l)
+		// day1.Task2(l)
+		// day2.Task1(l)
+		// day2.Task2(l)
+		// day3.Task2(l)
+		// day3.Task1(l)
+		//
+		// day4.Task1(l)
+		// day4.Task2(l)
 
-		day4.Task1(l)
-		day4.Task2(l)
+		day5.Task1(l)
 	},
 }
 
---
+++ day5/part1.go
@@ -0,0 +1,26 @@
+package day5
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/javorszky/adventofcode2022/inputs"
+	"github.com/rs/zerolog"
+)
+
+func Task1(l zerolog.Logger) {
+	localLogger := l.With().Int("day", 5).Int("part", 1).Logger()
+
+	gog, err := inputs.GroupByBlankLines("day5/input1.txt")
+	if err != nil {
+		localLogger.Err(err).Msg("could not read input file")
+		os.Exit(1)
+	}
+
+	// code goes here
+	fmt.Printf("top half goes here\n\n%#v", gog[0])
+
+	solution := 2
+	s := localLogger.With().Int("solution", solution).Logger()
+	s.Info().Msgf("-- change this for the part 1 message -- %d", solution)
+}
