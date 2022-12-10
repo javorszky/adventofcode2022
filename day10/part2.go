2219264f8fafbf531b7f5128ed00ce32b0cca40e
Gabor Javorszky <gabor@javorszky.co.uk>
2022-12-10 12:01:39 +0000

    Add day 10 part 2 solution

--- day10/part2.go
+++ day10/part2.go
@@ -2,23 +2,67 @@ package day10
 
 import (
 	"os"
+	"strings"
 
 	"github.com/javorszky/adventofcode2022/inputs"
 	"github.com/rs/zerolog"
 )
 
+const (
+	screenHeight = 6
+	screenWidth  = 40
+)
+
 func Task2(l zerolog.Logger) {
 	localLogger := l.With().Int("day", 10).Int("part", 2).Logger()
 
-	_, err := inputs.ReadIntoLines("day10/input2.txt")
+	gog, err := inputs.ReadIntoLines("day10/input1.txt")
 	if err != nil {
 		localLogger.Err(err).Msg("could not read input file")
 		os.Exit(1)
 	}
 
-	// code goes here
+	cycleValues := make([]int, 241)
+	cycleValues[0] = 1
 
-	solution := 2
-	s := localLogger.With().Int("solution", solution).Logger()
-	s.Info().Msgf("-- change this to the part 2 message -- %d", solution)
+	currentCycle := 0
+	currentRegister := 1
+	for _, line := range gog {
+		cycs, rise, err := parseCommand(line)
+		if err != nil {
+			localLogger.Err(err).Msgf("something went wrong with turning input to command")
+			os.Exit(1)
+		}
+		currentCycle++
+		cycleValues[currentCycle] = currentRegister
+		if cycs == 2 {
+			currentCycle++
+			cycleValues[currentCycle] = currentRegister
+		}
+		currentRegister += rise
+	}
+
+	localLogger.Debug().Msgf("%#v", cycleValues)
+
+	var sb strings.Builder
+	for row := 0; row < screenHeight; row++ {
+		for col := 0; col < screenWidth; col++ {
+			tick := row*screenWidth + col + 1
+			regVal := cycleValues[tick]
+			localLogger.Debug().Msgf("tick %d, regVal %d, col %d", tick, regVal, col)
+			switch {
+			case regVal == col || regVal-1 == col || regVal+1 == col:
+				localLogger.Debug().Msgf("--- writing #")
+				sb.WriteString("#")
+			default:
+				localLogger.Debug().Msgf("--- writing .")
+				sb.WriteString(".")
+			}
+		}
+		sb.WriteString("\n")
+	}
+
+	solution := sb.String()
+	s := localLogger.With().Str("solution", solution).Logger()
+	s.Info().Msgf("CRT display ends up being this:\n%s\n", solution)
 }
