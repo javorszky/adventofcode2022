a5edbc897d4850694cd8b7085b52b924cd1aa16e
Gabor Javorszky <gabor@suborbital.dev>
2022-12-15 09:31:08 +0000

    Add bounds for row for sensor

--- day15/sensor.go
+++ day15/sensor.go
@@ -1,5 +1,9 @@
 package day15
 
+import (
+	"fmt"
+)
+
 // coordinate holds a pair of ints, in col / row format.
 // x, y =>
 //
@@ -17,6 +21,16 @@ func (s sensor) rowInExclusion(row int) bool {
 	return s.self[1]-s.manhattanDistance <= row && row <= s.self[1]+s.manhattanDistance
 }
 
+func (s sensor) rowBoundCoordinates(row int) (coordinate, coordinate, error) {
+	if !s.rowInExclusion(row) {
+		return coordinate{}, coordinate{}, fmt.Errorf("row %d is not in exclusion zone for this sensor", row)
+	}
+
+	d := s.manhattanDistance - absDiff(row, s.self[1])
+
+	return coordinate{s.self[0] - d, row}, coordinate{s.self[0] + d, row}, nil
+}
+
 type grid struct {
 	sensors []sensor
 }
@@ -25,11 +39,23 @@ func (g grid) addSensor(s sensor) {
 	g.sensors = append(g.sensors, s)
 }
 
+func (g grid) sensorsExcludingRow(row int) []sensor {
+	listOfSensors := make([]sensor, 0)
+
+	for _, s := range g.sensors {
+		if s.rowInExclusion(row) {
+			listOfSensors = append(listOfSensors, s)
+		}
+	}
+
+	return listOfSensors
+}
+
 func newSensor(own, closest coordinate) sensor {
 	return sensor{
 		self:              own,
 		closestBeacon:     closest,
-		manhattanDistance: absDiff(own[0], closest[0]) + absDiff(own[1], closest[1]),
+		manhattanDistance: manhattanDistance(own, closest),
 	}
 }
 
@@ -43,4 +69,8 @@ func absDiff(a, b int) int {
 		return b - a
 	}
 	return c
+}
+
+func manhattanDistance(a, b coordinate) int {
+	return absDiff(a[0], b[0]) + absDiff(a[1], b[1])
 }
--- day15/sensor_test.go
+++ day15/sensor_test.go
@@ -1,6 +1,7 @@
 package day15
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -45,6 +46,65 @@ func Test_sensor_rowInExclusion(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := s.rowInExclusion(tt.row); got != tt.want {
 				t.Errorf("rowInExclusion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_sensor_rowBoundCoordinates(t *testing.T) {
+	s := newSensor(coordinate{10, 10}, coordinate{12, 12})
+
+	type args struct {
+		row int
+	}
+	tests := []struct {
+		name    string
+		row     int
+		want    coordinate
+		want1   coordinate
+		wantErr bool
+	}{
+		{
+			name:    "row goes through sensor",
+			row:     10,
+			want:    coordinate{6, 10},
+			want1:   coordinate{14, 10},
+			wantErr: false,
+		},
+		{
+			name:    "row is top tip",
+			row:     6,
+			want:    coordinate{10, 6},
+			want1:   coordinate{10, 6},
+			wantErr: false,
+		},
+		{
+			name:    "row is somewhere in the middle",
+			row:     8,
+			want:    coordinate{8, 8},
+			want1:   coordinate{12, 8},
+			wantErr: false,
+		},
+		{
+			name:    "row does not intersect",
+			row:     0,
+			want:    coordinate{},
+			want1:   coordinate{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := s.rowBoundCoordinates(tt.row)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("rowBoundCoordinates() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("rowBoundCoordinates() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("rowBoundCoordinates() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
